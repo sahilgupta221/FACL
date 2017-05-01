4f1dc7f712b380d3e4b1c8bdbd2caf1e323c6cb2
Sahil Gupta <sahilgupta221@gmail.com>
2017-04-30 20:13:45 -0400

    Added new files for creating application level handling of data. Routing need not to be changed. Everything should be handled at application level. Context analysis functionality will be added at this layer.

---
+++ applications/GuardACLTable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applications;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sahilgupta221
+ */
+public class GuardACLTable {
+    public String appid;
+    public ArrayList<String> function_list = null;
+    public long permission_code;
+}
---
+++ applications/RAEntry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applications;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sahilgupta221
+ */
+public class RAEntry {
+    public String appid;
+    public ArrayList<String> function_list = null;
+    public boolean isdelegated; 
+    public String delegatedfrom;
+}
