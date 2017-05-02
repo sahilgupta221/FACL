d4f7c0f0dde1ffa999adc5e058c8d801e767d20a
Sahil Gupta <sahilgupta221@gmail.com>
2017-05-01 23:45:32 -0400

    some files are added

---
+++ applications/RAapplication.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applications;
+
+import java.util.Random;
+
+import report.PingAppReporter;
+import core.Application;
+import core.DTNHost;
+import core.Message;
+import core.Settings;
+import core.SimClock;
+import core.SimScenario;
+import core.World;
+
+/**
+ *
+ * @author sahilgupta221
+ */
+public class RAapplication extends Application{
+
+    @Override
+    public Message handle(Message msg, DTNHost host) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(DTNHost host) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Application replicate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
